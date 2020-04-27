2fb61bfb8ebf6a6341b9f7f0f87bbe0cd8a89fee
royweb <xytank@gmail.com>
2020-04-27 14:04:13 -0400

    Set up unity and C# folders

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Access the Rigidbody property
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the cube
+        rb.AddForce(0, 0, 200);
+    }
+}
+
+// C# is Cap Sensitive
