f3e5dbf5e1ab97c889499af44274b5bbbced1d45
royweb <xytank@gmail.com>
2020-04-28 15:36:32 -0400

    Allow the player to move the cube to left and right

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,9 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     // float to work with decimal places
+    // f for float
     public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Update is called once per frame
     // FixedUpdate for dealing with physics
@@ -15,5 +17,17 @@ public class PlayerMovement : MonoBehaviour
         // Add force on the z-axis to move the cube
         // Time.deltaTime to fix the speed difference between the fast and slow pc
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        
+        // listen for keyborad
+        if(Input.GetKey("d"))
+        {
+            // move right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey("a"))
+        {
+            // move left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
