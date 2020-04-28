779d33e1ee285ab068d317538314528f20031189
royweb <xytank@gmail.com>
2020-04-28 14:02:22 -0400

    Change Update() to FixedUpdate() to improve performance when working with physics

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,13 +2,14 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Access the Rigidbody property
+    // Access the Rigidbody component
     public Rigidbody rb;
 
     // Update is called once per frame
-    void Update()
+    // FixedUpdate for dealing with physics
+    void FixedUpdate()
     {
-        // Move the cube
+        // Add force on the z-axis to move the cube
         // Time.deltaTime to fix the speed difference between the fast and slow pc
         rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
