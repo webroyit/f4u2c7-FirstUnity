859446a65282811b4c93caf6eb722e29c27f47a0
royweb <xytank@gmail.com>
2020-04-28 14:33:28 -0400

    Add a float variable

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,12 +5,15 @@ public class PlayerMovement : MonoBehaviour
     // Access the Rigidbody component
     public Rigidbody rb;
 
+    // float to work with decimal places
+    public float forwardForce = 2000f;
+
     // Update is called once per frame
     // FixedUpdate for dealing with physics
     void FixedUpdate()
     {
         // Add force on the z-axis to move the cube
         // Time.deltaTime to fix the speed difference between the fast and slow pc
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
     }
 }
