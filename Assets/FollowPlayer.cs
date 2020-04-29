6f2c0a3b25515f0118b623b4f99b3740f91113c9
royweb <xytank@gmail.com>
2020-04-29 13:33:32 -0400

    Add a class for follow player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Transform to access the player position, rotation and scale
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Print the value on the console
+        Debug.Log(player.position);
+    }
+}
