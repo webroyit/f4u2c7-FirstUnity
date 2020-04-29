1bd1c36a8597026c725e9f9619124abae22b9c20
royweb <xytank@gmail.com>
2020-04-29 14:09:28 -0400

    Move the camera near the player

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -4,11 +4,14 @@ public class FollowPlayer : MonoBehaviour
 {
     // Transform to access the player position, rotation and scale
     public Transform player;
+    // Vector3 stores three floats
+    public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-        // Print the value on the console
-        Debug.Log(player.position);
+        // Move the camera near the player
+        // offset values are from the camera input
+        transform.position = player.position + offset;
     }
 }
