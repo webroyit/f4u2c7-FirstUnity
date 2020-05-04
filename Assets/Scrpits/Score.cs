65f768937450233ccbdbcfde35a9cc9973a92d2f
royweb <xytank@gmail.com>
2020-05-04 15:42:08 -0400

    Add a class called score

---
+++ Assets/Scrpits/Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
