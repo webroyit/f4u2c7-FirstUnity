785b2c7f929d33f5b72f147824b804518c206f27
royweb <xytank@gmail.com>
2020-05-05 12:31:46 -0400

    Import UnityEngine.UI to change the UI text

--- Assets/Scrpits/Score.cs
+++ Assets/Scrpits/Score.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    // Access the transform property from player object
     public Transform player;
+    // Access the UI text
+    public Text scoreText;
     
     // Update is called once per frame
     void Update()
     {
-        
+        // Player score based on the value of the z-axis
+        Debug.Log(player.position.z);
     }
 }
