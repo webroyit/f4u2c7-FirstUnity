4cdda912a73a3f2c93e9144bb9632db849f0f0a6
royweb <xytank@gmail.com>
2020-05-06 13:56:50 -0400

    Restart the game if game over

--- Assets/Scrpits/GameManager.cs
+++ Assets/Scrpits/GameManager.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;      // For changing or reloading scene
 
 public class GameManager : MonoBehaviour
 {
     bool isGameOver = false;
 
+    // pubic to allow other files to access this function
     public void EndGame()
     {
         if(isGameOver == false)
         {
             isGameOver = true;
             Debug.Log("Game Over");
+            Restart();
         }
+    }
+
+    // Restart the game
+    void Restart()
+    {
+        // Reload the current scene or stage
+        // GetActiveScene().name to get the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
