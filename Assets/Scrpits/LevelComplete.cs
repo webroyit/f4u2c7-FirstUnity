d29c22c761a0c9bea2145d30d74c289063293979
royweb <xytank@gmail.com>
2020-05-08 13:04:04 -0400

    Add a function to load the next scene

--- Assets/Scrpits/LevelComplete.cs
+++ Assets/Scrpits/LevelComplete.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
-    public void LoadNextLevel(){
-        
+    public void LoadNextLevel()
+    {
+        // Get the current index from scenes in build(In Build Setting)
+        // Load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
