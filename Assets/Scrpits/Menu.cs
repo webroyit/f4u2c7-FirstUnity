607e415d6b833242e015c26df3c3ded639a2c85d
royweb <xytank@gmail.com>
2020-05-11 14:40:26 -0400

    Add a function to start the first level of the game

--- Assets/Scrpits/Menu.cs
+++ Assets/Scrpits/Menu.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    
+    public void StartGame()
+    {
+        // Get the current index from scenes in build(In Build Setting)
+        // Load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
